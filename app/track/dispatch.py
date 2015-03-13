c71f5653f2205a0b89be0ee5af81b5c63e90c521
Jasper Sibayan <sibayanjasper@gmail.com>
2015-03-13 16:58:19 +0800

    Fix error in tracks

--- app/track/dispatch.py
+++ app/track/dispatch.py
@@ -5,7 +5,6 @@ from flask import config, request, session, g
 
 # Import flask dependencies
 from flask import Blueprint, redirect, url_for, make_response
-from lib import res
 from lib.error_handler import FailedRequest
 
 
@@ -15,19 +14,19 @@ mod_tracks = Blueprint('track', __name__)
 
 # Set the route and accepted methods
 
-@mod_auth.route('/genre', methods=['GET'])
-def get_genre(query):
-    return res.send(auth.get_access_token(query))
+# @mod_auth.route('/genre', methods=['GET'])
+# def get_genre(query):
+#     return res.send(auth.get_access_token(query))
 
 
 
 
-@mod_auth.route('/artist', methods=['GET'])
-def get_artist(query):
-    return res.send(auth.get_access_token(query))
+# @mod_auth.route('/artist', methods=['GET'])
+# def get_artist(query):
+#     return res.send(auth.get_access_token(query))
 
 
 
-@mod_auth.route('/album', methods=['GET'])
-def get_album(query):
-    return res.send(auth.get_access_token(query))
+# @mod_auth.route('/album', methods=['GET'])
+# def get_album(query):
+#     return res.send(auth.get_access_token(query))
